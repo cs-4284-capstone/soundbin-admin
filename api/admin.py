075ca63b38511dffe9aa8d00c75d6d502d4def69
Wes <wesjordansemailaddress@gmail.com>
2019-03-22 17:57:43 -0400

    some models changes

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Track
+from .models import Track, Album
+
+
+class TrackInline(admin.StackedInline):
+    model = Track
+    extra = 1
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    inlines = [TrackInline]
+
 
 admin.site.register(Track)
+admin.site.register(Album, AlbumAdmin)
--- api/models.py
+++ api/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
 
-# Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField(default=5.99)
+    description = models.TextField()
+    releaseDate = models.DateField()
+    runtime_seconds = models.IntegerField(default=0)
+    runtime_minutes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.title} ({self.releaseDate.year})"
+
+
 class Track(models.Model):
     title = models.CharField(max_length=200)
-    price = models.FloatField()
-    albumId = models.IntegerField()
-    albumTitle = models.CharField(max_length=200)
-    runtimeSeconds = models.IntegerField()
-    runtimeMinutes = models.IntegerField()
+    price = models.FloatField(default=0.99)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    runtime_seconds = models.IntegerField(default=0)
+    runtime_minutes = models.IntegerField(default=0)
+    #file = models.FileField()
+
+    def __str__(self):
+        return f"{self.title} ({self.album.title})"
