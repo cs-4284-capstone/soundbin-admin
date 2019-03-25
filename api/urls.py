293af1c665b0c50cb65377692783aa457af5d36f
Brian Van Rosendale <GrnDyRx@gmail.com>
2019-03-25 18:44:48 -0400

    added user association api

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('tracks/<int:id>', views.track, name='track-single'),
 
     path('albums/', views.albums, name='albums-all'),
-    path('albums/<int:id>', views.album, name='album-single')
+    path('albums/<int:id>', views.album, name='album-single'),
+
+    path('associate/', views.associate, name='associate-user')
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 
+import json
 
 # Create your views here.
 from .models import Track, Album
@@ -32,3 +34,17 @@ def album(request, id):
     body = q.to_json()
     print(body)
     return JsonResponse(body)
+
+@csrf_exempt
+def associate(request):
+    if request.method != 'POST':
+        print('Invalid non-post call to associate')
+        return HttpResponse('Invalid request type, must be post', status=400)
+
+    data = json.loads(request.body)
+    print(f'Recieved data: (wallet:{data["walletid"]}, email:{data["email"]})')
+
+    if True:    # TODO: attempt to add to database
+        return HttpResponse('Wallet and email associated', status=200)
+    else:
+        return HttpResponse('Unknown error', status=500)
