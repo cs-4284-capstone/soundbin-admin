30e7c09c7a2bd20d9dded97b81438ad8dc59666d
Brian Van Rosendale <GrnDyRx@gmail.com>
2019-03-26 14:48:18 -0400

    work with firefox send

--- api/song_emailer/send_song.py
+++ api/song_emailer/send_song.py
@@ -10,22 +10,34 @@ import argparse
 import ssl
 import smtplib
 import configparser
+import os
+import subprocess
 
 def get_message(links, config, src_email, dest_email):
     """format proper message"""
     message = ''
 
-    message += f'To: {dest_email}'
-    message += f'From: {config["Email"]["name"]} <{src_email}>'
+    message += f'To: {dest_email}\n'
+    message += f'From: {config["Email"]["name"]} <{src_email}>\n'
     message += 'Subject: {config["Email"]["subject"]}\n\n'
     message += 'Thank you for purchasing music. Here are the download links:\n\n'
     message += '\n'.join(links)
 
     return message
 
-def firefox_send(links):
-    """process a list and get a link"""
-    #TODO: implement firefox send
+def firefox_send(files):
+    """get firefox send links for each file"""
+    links = []
+    for song_file in files:
+        args = ['send-cli', song_file]
+        firefox_send = subprocess.run(args, capture_output=True)
+        output_lines = firefox_send.stdout.decode('utf-8').split('\n')
+        link = output_lines[-3]
+        print(firefox_send)
+        print(output_lines)
+        print(link)
+        links.append(link)
+
     return links
 
 def links_from_songs(songs, files_config):
@@ -33,17 +45,19 @@ def links_from_songs(songs, files_config):
     links = []
 
     # for now, just combine the base url and the song name
-    base = '.' + files_config['song_dir']
+    base = files_config['song_dir']
     for song in songs:
-        links.append(base + song)
+        links.append(os.path.join(os.path.dirname(__file__), base, song))
 
     return firefox_send(links)
 
 def main():
     """Deal with input (called from command line by other software)"""
+    config_filename = os.path.join(os.path.dirname(__file__), 'config.ini')
+
     # load config
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read(config_filename)
 
     # parse command line arguments
     parser = argparse.ArgumentParser(description='Send purchased songs to an email')
@@ -56,14 +70,17 @@ def main():
 
     files_config = config['Files']
 
+    # load email credentials
+    email_credentials_file = os.path.join(os.path.dirname(__file__),
+                                          'email_credentials.txt')
+
+    with open(email_credentials_file, 'r') as f:
+        src_email = f.readline()
+        password = f.readline()
+
     links = links_from_songs(songs, files_config)
     message = get_message(links, config, src_email, dest_email)
 
-    # load email credentials
-    with open('email_credentials.txt', 'r') as f:
-        src_email = f.readline()
-        password = f.readline()
-    
     # establish connection to smtp server
     context = ssl.create_default_context()
 
